c36f470a65215c20fb1a7b0359164b12b18699dc
pbberlin <peter.buchmann@web.de>
2016-07-20 22:08:02 +0200

    inital

---
+++ errors.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"os"
+	"strings"
+
+	"github.com/zew/logx"
+)
+
+func CheckErr(err error) {
+	defer logx.SL().Incr().Decr()
+	if err != nil {
+		logx.Printf("%v", err)
+		str := strings.Join(logx.StackTrace(2, 3, 2), "\n")
+		logx.Printf("\nStacktrace: \n%s", str)
+		os.Exit(1)
+	}
+}
