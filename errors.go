6a96c19ea9920bded7fab735e328c07bf7cf554d
pbberlin <peter.buchmann@web.de>
2016-07-21 15:35:29 +0200

    tolerable errors

--- errors.go
+++ errors.go
@@ -7,9 +7,17 @@ import (
 	"github.com/zew/logx"
 )
 
-func CheckErr(err error) {
+func CheckErr(err error, tolerate ...string) {
 	defer logx.SL().Incr().Decr()
 	if err != nil {
+		errStr := strings.ToLower(err.Error())
+		for _, tol := range tolerate {
+			tol = strings.ToLower(tol)
+			if strings.Contains(errStr, tol) {
+				logx.Printf("tolerated error: %v", err)
+				return
+			}
+		}
 		logx.Printf("%v", err)
 		str := strings.Join(logx.StackTrace(2, 3, 2), "\n")
 		logx.Printf("\nStacktrace: \n%s", str)
