323511dbd176cfb0079da148b4e50c4643950cb4
pbberlin <peter.buchmann@web.de>
2018-09-17 15:29:06 +0200

    fixed the parsing of command line flags

--- loadconfig.go
+++ loadconfig.go
@@ -77,12 +77,13 @@ func LoadConfigFile(fName string, optSubdir ...string) (io.ReadCloser, error) {
 
 // Example: FlagVal("configfile", "config.json", "cfg", "JSON file containing config data")
 type FlagT struct {
-	Long       string
-	Short      string
+	Long       string // key, such as config_file, overrides Short key
+	Short      string // short key, such as cfg
 	DefaultVal string
-	Desc       string
+	Desc       string // description, printed on executable -h
 
-	Val string // computed
+	Val   string // computed
+	valSh string // possible value from the short key;
 }
 
 type FlagsT []FlagT
@@ -104,24 +105,37 @@ func (f *FlagsT) Add(a FlagT) {
 // Command line flag overrides environment variable
 func (f *FlagsT) Gen() {
 
-	for idx, ff := range *f {
-		val := ""
-		// i.e. --configfile file1.json
-		// or   --configfile=file1.json
-		flag.StringVar(&val, ff.Long, "", ff.Desc)
-		// i.e. -cfg file2.json
-		// or   -cfg=file2.json
-		flag.StringVar(&val, ff.Short, "", ff.Desc+", shorthand")
-		(*f)[idx].Val = val
+	// Oh god!
+	// But there is no other way get pointers
+	// into	our slice of FlagT structs.
+	for idx := range *f {
+		//    -cfg        config.json
+		//    -lgn        logins.json
+		flag.StringVar(&(*f)[idx].Val, (*f)[idx].Long, "", (*f)[idx].Desc)
+		//    -config_file config.json
+		//    -logins_file logins.json
+		flag.StringVar(&(*f)[idx].valSh, (*f)[idx].Short, "", (*f)[idx].Desc+", shorthand")
 	}
 
 	flag.Parse() // Parse and register all flags
+
+	//
+
+	if len(flag.Args()) > 0 {
+		logx.Printf("UNRECOGNIZED command line arguments: %v", flag.Args())
+	}
 
 	// Loop again for ENV variable fallback
 	// and default value fallback.
 	for idx, ff := range *f {
 
 		val := (*f)[idx].Val
+		valSh := (*f)[idx].valSh
+
+		if val == "" && valSh != "" {
+			val = valSh
+			logx.Printf("Taking short key val, since long key is empty %v / %v => %q", ff.Long, ff.Short, val)
+		}
 
 		if val == "" {
 			uLong := strings.ToUpper(ff.Long)
@@ -132,10 +146,11 @@ func (f *FlagsT) Gen() {
 		}
 
 		if val == "" {
+			logx.Printf("Taking %v from DEFAULT: %v", ff.Long, ff.DefaultVal)
 			val = ff.DefaultVal
 		}
 
-		logx.Printf("flag %v = %v", ff.Long, val)
+		logx.Printf("Effective %v / %v => %q", ff.Long, ff.Short, val)
 
 		(*f)[idx].Val = val
 	}
